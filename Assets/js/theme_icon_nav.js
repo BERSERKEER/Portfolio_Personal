f1f1be2bbce9d5061dbafa2b777ec64d2e292edb
Johan <calderonjohan604@gmail.com>
2025-02-07 10:09:51 -0500

    Ajuste visuales

---
+++ Assets/js/theme_icon_nav.js
@@ -0,0 +1,14 @@
+
+function changeFavicon() {
+    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const favicon = document.getElementById('favicon');
+    
+    if (darkModeMediaQuery.matches) {
+      favicon.href = '../../assets/images/icon/icon_portfolio_light.svg'; // Icon dark theme
+    } else {
+      favicon.href = '../../assets/images/icon/icon_portfolio_dark.svg'; // Icon light theme
+    }
+  }
+
+  window.onload = changeFavicon;
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', changeFavicon);
--- Config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$folderPath = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
-$urlPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$url = substr($urlPath, strlen($folderPath));
-
-define('URL', trim($url, '/')); // Elimina posibles barras al inicio o final.
