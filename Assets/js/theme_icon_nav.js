a48592b8e71e0d2ece497fa225a8e3ddc03059e4
Johan <calderonjohan604@gmail.com>
2025-03-12 11:46:51 -0500

    Fix favicon paths and update image source references in HTML files

--- Assets/js/theme_icon_nav.js
+++ Assets/js/theme_icon_nav.js
@@ -1,13 +1,13 @@
 function changeFavicon() {
-    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-    const favicon = document.getElementById('favicon');
-    
-    if (darkModeMediaQuery.matches) {
-      favifaviconon.href = '/assets/images/icon/icon_portfolio_dark.svg'; // Icon dark theme
-    } else {
-      favicon.href = '/assets/images/icon/icon_portfolio_light.svg'; // Icon light theme
-    }
+  const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+  const favicon = document.getElementById('favicon');
+  
+  if (darkModeMediaQuery.matches) {
+    favicon.href = '/Assets/images/icon/icon_portfolio_dark.svg'; // Icon dark theme
+  } else {
+    favicon.href = '/Assets/images/icon/icon_portfolio_light.svg'; // Icon light theme
   }
+}
 
-  window.onload = changeFavicon;
-  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', changeFavicon);
+window.onload = changeFavicon;
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', changeFavicon);
